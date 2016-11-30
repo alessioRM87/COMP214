ba472bcfb3922a0906b726138c76c2c2e7d6d31d
alessioRM87 <alessio.iannella@gmail.com>
2016-11-29 22:05:31 -0500

    DATABASE MANAGER
    Added database manager and getEmployees method

---
+++ GP29/App_Code/MANAGERS/DatabaseManager.cs
@@ -0,0 +1,65 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descrizione di riepilogo per DatabaseManager
+/// </summary>
+public class DatabaseManager
+{
+
+    #region AutoProperties
+
+    private OracleConnection connection { get; set; }
+
+    #endregion
+
+    #region Variable Declaration
+
+    private const String connectionString = "Dorknozzle";
+
+    #endregion
+
+    #region Constructor
+    public DatabaseManager()
+    {
+        connection = new OracleConnection(ConfigurationManager.ConnectionStrings[connectionString].ConnectionString);
+    }
+
+    #endregion
+
+    #region Methods
+
+    public void getEmployees(Action<DataSet> success, Action failed)
+    {
+        OracleCommand oracleCommand = new OracleCommand("SELECT * FROM EMPLOYEE", connection);
+
+        try
+        {
+            connection.Open();
+
+            OracleDataAdapter oracleDataAdapter = new OracleDataAdapter(oracleCommand);
+
+            DataSet dataSet = new DataSet();
+
+            oracleDataAdapter.Fill(dataSet);
+
+            success.Invoke(dataSet);
+
+        }
+        catch
+        {
+            failed.Invoke();
+        }
+        finally
+        {
+            connection.Close();
+        }
+    }
+
+    #endregion
+}
